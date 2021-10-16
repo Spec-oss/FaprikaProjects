1632a982115aec2747221547537ddbdf19f6dee1
Akın Turan <akin.turan@outlook.com.tr>
2021-10-16 23:08:01 +0300

    mvc

---
+++ Core/DataAccess/Dapper/DapperEntityRepositoryBase.cs
@@ -0,0 +1,41 @@
+﻿using Core.Entity;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Dapper
+{
+    public class DapperEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DataAccess/Concrete/Dapper/FaprikaProjectContext.cs
+++ DataAccess/Concrete/Dapper/FaprikaProjectContext.cs
@@ -6,11 +6,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using Microsoft.Data.SqlClient;
+using System.Data.Common;
 
 namespace DataAccess.Concrete.Dapper
 {
     public class FaprikaProjectContext
     {
-        SqlConnection conn = new SqlConnection(@"Server=DESKTOP-EOISGJ5;Database=FaprikaProject;Trusted_Connection=true");
+        public static DbConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection(@"Server=DESKTOP-EOISGJ5;Database=FaprikaProject;Trusted_Connection=true");
+            connection.Open();
+            return connection;
+        }
     }
 }
