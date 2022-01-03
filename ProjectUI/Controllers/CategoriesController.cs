7dd6e03741f05da364c85345a2646ca04893c1b4
Akın Turan <akin.turan@outlook.com.tr>
2022-01-03 22:22:15 +0300

    CategoriesController & View added

---
+++ ProjectUI/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+﻿using Bussiness.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(IProductService productService,ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+        }
+        public IActionResult Categories()
+        {
+            var model = _categoryService.GetAll();
+            return View(model.Data);
+        }
+        public IActionResult CategoryDetails(int id)
+        {
+            var categoryid = _productService.GetProductByCategoryId(id);
+            return View(categoryid.Data);
+            
+        }
+    }
+}
